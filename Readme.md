9a7ec541777d53d8203d1c15012b4c5d23331acd
dennisliuu <dennisliuu@gmail.com>
2019-11-04 02:23:52 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# WordCloud + Ckiptagger + 詩經
+
+Use ckip analysis as tokenizer for 詩經
+And display in word cloud.
+
+/ckip:
+```
+python main.py # Get tokenizer for 詩經 and store as tokenize.txt
+```
+
+Usage:
+```
+python main.py
+```
---
+++ ckip/main.py
@@ -0,0 +1,12 @@
+text = open('SiGing.txt', 'r',encoding= 'UTF-8-sig').read()
+# print(text)
+
+from ckiptagger import data_utils, construct_dictionary, WS, POS, NER
+
+#data_utils.download_data_gdown("./")
+ws = WS("./data")
+ws_results = ws([text])
+# print(ws_results)
+
+f = open("tokenize.txt", "w+")
+f.write("%s" % ws_results[0])
